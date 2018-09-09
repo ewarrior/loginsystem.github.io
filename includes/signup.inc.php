4a7116325b8e2846b8cf68caf4ce1099d8d57bb8
anugup7 <anuragupta.135@gmail.com>
2018-09-09 18:13:19 +0530

    Initial commit

---
+++ includes/signup.inc.php
@@ -0,0 +1,65 @@
+<?php
+
+
+if (isset($_POST['submit'])) {
+
+	include_once 'dbh.inc.php';
+
+	$first = mysqli_real_escape_string($conn, $_POST['first']);
+	$last = mysqli_real_escape_string($conn, $_POST['last']);
+	$email = mysqli_real_escape_string($conn, $_POST['email']);
+	$uid = mysqli_real_escape_string($conn, $_POST['uid']);	
+	$pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+    $phn = mysqli_real_escape_string($conn, $_POST['phn']);
+
+	//Error Handlers
+	//Check for empty fields
+	if(empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd) || empty($phn))
+	 {
+
+			header("Location: ../signup.php?signup=empty");
+			exit();
+
+	}else {
+
+		if(!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last))
+		{
+
+			header("Location: ../signup.php?signup=invalid");
+			exit();
+		}
+		else {
+			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+			header("Location: ../signup.php?signup=invalid-email");
+			exit();
+			}
+			else {
+				$sql = "SELECT * FROM users WHERE user_uid='$uid'";
+				$result = mysqli_query($conn, $sql);
+				$resultCheck = mysqli_num_rows($result);
+
+				if($resultCheck > 0)
+				{
+
+			header("Location: ../signup.php?signup=empty");
+			exit();
+				} else {
+					$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+					$sql ="INSERT INTO `users` (`user_id`, `user_first`, `user_last`, `user_email`, `user_uid`, `user_pwd`, `phn`) VALUES (NULL, '$first', '$last', '$email', '$uid', '$pwd', '$phn');";
+					mysqli_query($conn, $sql);
+				
+			header("Location: ../signup.php?signup=success");
+			exit();
+
+
+				}
+			}
+		}
+	}
+}
+else {
+	header("Location: ../signup.php");
+	exit();
+}
+
